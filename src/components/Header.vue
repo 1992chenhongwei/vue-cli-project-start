3bd539af4a732e0f5f985408369d223513593cdc
1992chenhongwei <feng992211@163.com>
2018-05-02 15:03:57 +0800

    font-awsome

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
         <router-link class="py-2" to="/">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
         </router-link>
-        <router-link to='homeLink' class="navbar-brand">Pizza点餐系统</router-link>
+        <router-link to='homeLink' class="navbar-brand"><i class="fa fa-camera-retro"></i>Pizza点餐系统</router-link>
         <ul class="navbar-nav">
           <!-- tag属性可以定义router-link所使用的标签，默认为a标签 -->
           <!-- <li><router-link to="/" tag="div" class="nav-link">主页</router-link></li> -->
---
+++ src/css/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: ChenHongwei
+* @Date:   2017-05-09 16:48:55
+* @Last Modified by:   ChenHongwei
+* @Last Modified time: 2017-05-17 21:20:30
+*/
+
+'use strict'
+
+import './layout.css'
+import './css/font-awesome.min.css'
